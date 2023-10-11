447ea13ec4a2ed5713e55e5b3b09d318fba66e76
Elena <aleklenoch@gmail.com>
2023-10-11 21:08:13 +0300

    #model RegistrModel

--- GestBookRazorPages/Pages/Registration.cshtml.cs
+++ GestBookRazorPages/Pages/Registration.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using GestBookRazorPages.Repositpry;
+using GestBookRazorPages.Models;
 namespace GestBookRazorPages.Pages
 {
     public class RegistrationModel : PageModel
     {
+        public RegisterModel reg { get; set; }
         public void OnGet()
         {
         }
