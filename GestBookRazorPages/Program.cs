5e4a3b33cd7116e693d21a6fe94b5f03aba5c0a5
alekLen <aleklenoch@gmail.com>
2024-04-26 13:31:12 +0300

    #подключение без пароля

--- GestBookRazorPages/Program.cs
+++ GestBookRazorPages/Program.cs
@@ -4,7 +4,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Получаем строку подключения из файла конфигурации
-string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+
+//string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // добавляем контекст ApplicationContext в качестве сервиса в приложение
 builder.Services.AddDbContext<GestBookContext>(options => options.UseSqlServer(connection));
