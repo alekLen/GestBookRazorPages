8afdcaeb166243fa8935c600fcd4d487700ede26
alekLen <aleklenoch@gmail.com>
2024-04-26 18:17:37 +0300

    #Index

--- GestBookRazorPages/Program.cs
+++ GestBookRazorPages/Program.cs
@@ -41,6 +41,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else { 
+app.UseWelcomePage("/Index");}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
