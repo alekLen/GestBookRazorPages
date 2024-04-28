a35507ca0fd357aa484ec88323a55da3ec195bde
alekLen <aleklenoch@gmail.com>
2024-04-28 21:18:11 +0300

    #вернула БД

--- GestBookRazorPages/Pages/Index.cshtml.cs
+++ GestBookRazorPages/Pages/Index.cshtml.cs
@@ -7,18 +7,18 @@ namespace GestBookRazorPages.Pages
 {
     public class IndexModel : PageModel
     {
-       // IRepository rep;
-       //public List<Message> list{set; get; }
-       // public Message message { set; get; }
-       // public IndexModel(IRepository context)
-       // {
-       //     rep = context;
-       // }
+        IRepository rep;
+        public List<Message> list { set; get; }
+        public Message message { set; get; }
+        public IndexModel(IRepository context)
+        {
+            rep = context;
+        }
 
         public async Task<IActionResult> OnGet()
         {
-           // var ms = await rep.GetMessage();
-           //list = ms;
+            var ms = await rep.GetMessage();
+            list = ms;
             return Page();
         }
     }
