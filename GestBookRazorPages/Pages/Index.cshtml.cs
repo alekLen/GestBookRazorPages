eb8a1d3f0d0ed47688cd88de7c76a1e1471e95df
alekLen <aleklenoch@gmail.com>
2024-05-08 16:04:29 +0300

    #новый пользователь БД

--- GestBookRazorPages/Pages/Index.cshtml.cs
+++ GestBookRazorPages/Pages/Index.cshtml.cs
@@ -10,39 +10,14 @@ namespace GestBookRazorPages.Pages
         IRepository? rep;
         public List<Message> list { set; get; }
         public Message message { set; get; }
-        public IndexModel()
-        {
-            try
-            {
-                list = new List<Message>();
-                message = new Message();
-                message.Text = "конструктор без параметров ";
-               
-            }
-            catch (Exception ex)
-            {
-                list = new List<Message>();
-                message = new Message();
-                message.MessageDate = "0/0/0";
-                message.Text = "конструктор  " + ex.Message;
-                User u = new User();
-                u.Name = "admin";
-                message.user = u;
-                list.Add(message);
-            }
-        }
-        //public IndexModel(IRepository context)
+        //public IndexModel()
         //{
         //    try
         //    {
         //        list = new List<Message>();
         //        message = new Message();
-        //        message.Text = "конструктор с параметром ";
-        //        User u = new User();
-        //        u.Name = "admin";
-        //        message.user = u;
-        //        list.Add(message);
-        //        // rep = context;
+        //        message.Text = "конструктор без параметров ";
+               
         //    }
         //    catch (Exception ex)
         //    {
@@ -56,49 +31,74 @@ namespace GestBookRazorPages.Pages
         //        list.Add(message);
         //    }
         //}
+        public IndexModel(IRepository context)
+        {
+            try
+            {
+                list = new List<Message>();
+                message = new Message();
+                message.Text = "конструктор с параметром ";
+                User u = new User();
+                u.Name = "admin";
+                message.user = u;
+                list.Add(message);
+                rep = context;
+            }
+            catch (Exception ex)
+            {
+                list = new List<Message>();
+                message = new Message();
+                message.MessageDate = "0/0/0";
+                message.Text = "конструктор  " + ex.Message;
+                User u = new User();
+                u.Name = "admin";
+                message.user = u;
+                list.Add(message);
+            }
+        }
 
         public async Task<IActionResult> OnGet()
         {
-            //try
-            //{
-            //    if (rep == null)
-            //    {
+            try
+            {
+                if (rep == null)
+                {
 
 
-            //        message.MessageDate = "00/00/00";
-            //        message.Text += "репозиторий !";
-            //        User u = new User();
-            //        u.Name = "admin";
-            //        message.user = u;
-            //        list.Add(message);
-            //    }
-            //    else
-            //    {
+                    message.MessageDate = "00/00/00";
+                    message.Text += "репозиторий !";
+                    User u = new User();
+                    u.Name = "admin";
+                    message.user = u;
+                    list.Add(message);
+                }
+                else
+                {
 
-            //        var ms = await rep.GetMessage();
-            //        list = ms;
-            //        if (ms.Count == 0)
-            //        {
+                    var ms = await rep.GetMessage();
+                    list = ms;
+                    if (ms.Count == 0)
+                    {
 
-            //            message.MessageDate = "00/00/00";
-            //            message.Text += "Пусто";
-            //            User u = new User();
-            //            u.Name = "admin";
-            //            message.user = u;
-            //            list.Add(message);
-            //        }
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
+                        message.MessageDate = "00/00/00";
+                        message.Text += "Пусто";
+                        User u = new User();
+                        u.Name = "admin";
+                        message.user = u;
+                        list.Add(message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
 
-            //    message.MessageDate = "0/0/0";
-            //    message.Text = ex.Message;
-            //    User u = new User();
-            //    u.Name = "admin";
-            //    message.user = u;
-            //    list.Add(message);
-            //}
+                message.MessageDate = "0/0/0";
+                message.Text = ex.Message;
+                User u = new User();
+                u.Name = "admin";
+                message.user = u;
+                list.Add(message);
+            }
 
             return Page();
         }
